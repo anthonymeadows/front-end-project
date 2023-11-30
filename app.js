094b8698c9241e52a16631040c6ea21f6ecdcd43
unknown <anthony.meadows2000@outlook.com>
2023-11-30 11:16:27 -0700

    added README, Fully working

--- app.js
+++ app.js
@@ -1,179 +1,197 @@
-$(document).ready(function () {;
-    $('button').click(fetchData);
+$(document).ready(function () {
+    $('button').click(match);
 });
 
-let arrayOfImages = [];
-let arrayOfImageObjects = [];
-let selectedCard;
-let lastCard;
-let clickedCards = [];
-let found = 0;
+$('#score').appendTo($('#header'))
 
-async function fetchData() {
-    gameStart()
-    const pairRequest = parseInt($('input').val(), 10);
+function match() {
 
-    // Clear the previous game elements
-    $('#howManyPairs').remove();
+    let arrayOfImages = [];
+    let arrayOfImageObjects = [];
+    let selectedCard;
+    let lastCard;
+    let clickedCards = [];
+    let found = 0;
 
-    if (!isNaN(pairRequest) && pairRequest > 0) {
-        try {
-            // Fetch unique images
-            while (arrayOfImages.length < pairRequest) {
-                const data = await fetchSingleImage();
+    async function fetchData() {
+        gameStart();
+        const pairRequest = parseInt($('#userInput').val(), 10);
+        $('#howManyPairs').toggle();
 
-                // Check for duplicates in arrayOfImages
-                if (!arrayOfImages.includes(data)) {
-                    arrayOfImages.push(data);
+        if (!isNaN(pairRequest) && pairRequest > 0) {
+            try {
+                while (arrayOfImages.length < pairRequest) {
+                    const data = await fetchSingleImage();
+                    if (!arrayOfImages.includes(data)) {
+                        arrayOfImages.push(data);
+                    }
                 }
+            } catch (error) {
+                console.error(error);
             }
-        } catch (error) {
-            console.error(error);
+        }
+
+        handleImages();
+        score();
+    }
+
+    async function fetchSingleImage() {
+        return new Promise((resolve, reject) => {
+            $.get('https://random.imagecdn.app/v1/image?width=120&height=120', (data) => {
+                resolve(data);
+            }).fail((error) => {
+                reject(error);
+            });
+        });
+    }
+
+    //Used to identify matches
+    function assignCardNum() {
+        for (let i = 0; i < arrayOfImages.length; i++) {
+            let obj = {};
+            obj['card'] = i;
+            obj['url'] = arrayOfImages[i];
+            arrayOfImageObjects.push(obj);
         }
     }
 
-    // Start or restart the game
-    handleImages();
-}
+    //double the array, and shuffle the order using fisher yates algorithm
+    function shuffleAndDouble() {
+        assignCardNum();
+        arrayOfImageObjects = arrayOfImageObjects.concat(arrayOfImageObjects);
+        for (let i = arrayOfImageObjects.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [arrayOfImageObjects[i], arrayOfImageObjects[j]] = [arrayOfImageObjects[j], arrayOfImageObjects[i]];
+        }
+    }
 
-async function fetchSingleImage() {
-    return new Promise((resolve, reject) => {
-        $.get('https://random.imagecdn.app/v1/image?width=120&height=120', (data) => {
-            resolve(data);
-        }).fail((error) => {
-            reject(error);
+    //holds all cards
+    function cardHolder() {
+        $('#cardsHolder').css({
+            position: 'absolute',
+            top: '100px',
+            backgroundColor: '#07125b',
+            display: 'flex',
+            flexWrap: 'wrap',
+            padding: '10px',
+            justifyContent: 'center',
+            border: '5px solid black',
+            borderRadius: '10px',
+            height: 'max-content',
+            width: '90dvw',
+            gap: '1rem',
+            zIndex: '0',
         });
-    });
-}
-
-function assignCardNum() {
-    for (let i = 0; i < arrayOfImages.length; i++) {
-        let obj = {};
-        obj['card'] = i;
-        obj['url'] = arrayOfImages[i];
-        arrayOfImageObjects.push(obj);
     }
-}
 
-function shuffleAndDouble() {
-    assignCardNum();
-    // Duplicate each element in the array
-    arrayOfImageObjects = arrayOfImageObjects.concat(arrayOfImageObjects);
-    // Fisher-Yates algorithm for shuffling
-    for (let i = arrayOfImageObjects.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [arrayOfImageObjects[i], arrayOfImageObjects[j]] = [arrayOfImageObjects[j], arrayOfImageObjects[i]];
+    function handleImages() {
+        shuffleAndDouble();
+        cardHolder();
+        $('#cardsHolder').empty();
+
+        for (let i = 0; i < arrayOfImageObjects.length; i++) {
+            createCard(arrayOfImageObjects[i]);
+        }
+        gameLogic();
     }
-}
 
-function cardHolder() {
-    $('#cardsHolder').css({
-        position: 'absolute',
-        top: '100px',
-        backgroundColor: '#07125b',
-        display: 'flex',
-        flexWrap: 'wrap',
-        padding: '10px',
-        justifyContent: 'center',
-        border: '5px solid black',
-        borderRadius: '10px',
-        height: 'max-content',
-        width: '90vw', // Corrected the typo in '90dvw' to '90vw'
-        gap: '1rem',
-        zIndex: '0',
-    });
-}
+    function createCard(cardData) {
+        let imageUrl = cardData['url'];
+        let pairNum = cardData['card'];
 
-function handleImages() {
-    shuffleAndDouble();
-    cardHolder();
-
-    // Clear the existing content in the #cardsHolder
-    $('#cardsHolder').empty();
-    for (let i = 0; i < arrayOfImageObjects.length; i++) {
-        let imageUrl = arrayOfImageObjects[i]['url'];
-        let pairNum = arrayOfImageObjects[i]['card'];
-
-        // Create a card container
         let div = $('<div>').addClass('card').attr('data-pair', pairNum);
-
-        // Create elements for front and back of the card
         let front = $('<img>').addClass('front').attr('src', 'frontCard.png').appendTo(div);
         let back = $('<img>').addClass('back').attr('src', imageUrl).appendTo(div);
 
-        // Keep track of the card state and the currently displayed image
-        div.click(function () {
+        div.click(function (e) {
+            e.preventDefault()
             $(this).toggleClass('flipped');
-            eventHandlers(pairNum);
+            gameLogic(pairNum);
         });
 
-        // Append the div to the #cardsHolder
         div.appendTo($('#cardsHolder'));
     }
-    eventHandlers();
-}
 
-function eventHandlers(pairNum) {
-    let flippedCards = $('.flipped:not(.matched)');
+    function gameLogic(pairNum) {
+        let flippedCards = $('.flipped:not(.matched)');
 
-    if (flippedCards.length === 2) {
-        // Disable further clicks during the comparison
-        $('#cardsHolder').css('pointer-events', 'none');
+        if (flippedCards.length === 2) {
+            $('#cardsHolder').css('pointer-events', 'none');
 
-        let card1 = $(flippedCards[0]);
-        let card2 = $(flippedCards[1]);
+            let card1 = $(flippedCards[0]);
+            let card2 = $(flippedCards[1]);
 
-        if (card1.attr('data-pair') === card2.attr('data-pair')) {
-            // Matched
-            flippedCards.addClass('matched');
-            found++;
-
-            // Play match sound
-            playMatchSound();
-
-            // Check if all pairs are found
-            if (found === arrayOfImages.length) {
-                // All pairs found, handle game end
-                playAllFound()();
+            if (card1.attr('data-pair') === card2.attr('data-pair')) {
+                handleMatchedCards(flippedCards);
+            } else {
+                handleMismatchedCards(flippedCards);
             }
-
-            setTimeout(() => {
-                // Replace the content with a blank div
-                flippedCards.html('<div class="blank"></div>');
-                // Enable further clicks after replacing content
-                $('#cardsHolder').css('pointer-events', 'auto');
-            }, 1000);
-        } else {
-            wrongChoice()
-            // Not matched, flip back after a delay
-            setTimeout(() => {
-                flippedCards.removeClass('flipped');
-                // Enable further clicks after flipping back
-                $('#cardsHolder').css('pointer-events', 'auto');
-            }, 1000);
         }
     }
-}
+
+    function handleMatchedCards(flippedCards) {
+        flippedCards.addClass('matched');
+        found++;
+        score();
+        playMatchSound();
+
+        if (found === arrayOfImages.length) {
+            playAllFound();
+            playAgain();
+        }
+
+        setTimeout(() => {
+            flippedCards.html('<div class="blank"></div>');
+            $('#cardsHolder').css('pointer-events', 'auto');
+        }, 1000);
+    }
+
+    function handleMismatchedCards(flippedCards) {
+        wrongChoice();
+
+        setTimeout(() => {
+            flippedCards.removeClass('flipped');
+            $('#cardsHolder').css('pointer-events', 'auto');
+        }, 1000);
+    }
+
+    function score() {
+        $('#score').text(`${found} / ${arrayOfImages.length}`)
+    }
+
+    function playAgain() {
+        arrayOfImages = [];
+        arrayOfImageObjects = [];
+        selectedCard = undefined;
+        lastCard = undefined;
+        clickedCards = [];
+        found = 0;
+        $('#howManyPairs').toggle();
+        handleImages();
+        score();
+    }
+
+    fetchData();
+};
+
+//Audio elements
 
 function playMatchSound() {
-    // Get the audio element
-    let matchSound = document.getElementById('matchSound');
-
-    // Play the audio
+    let matchSound = $('#matchSound')[0];
     matchSound.play();
 }
 
 function playAllFound() {
-    let allFound = document.getElementById('allFound')
+    let allFound = $('#allFound')[0];
     allFound.play();
 }
 
 function gameStart() {
-    let gameStart = document.getElementById('gameStart')
+    let gameStart = $('#gameStart')[0];
     gameStart.play();
 }
 
 function wrongChoice() {
-    let wrongChoice = document.getElementById('wrongChoice')
+    let wrongChoice = $('#wrongChoice')[0];
     wrongChoice.play();
 }
